de72367a588362177f6637f35428646ad1531c5a
Zutanoo <zutano6076@gmail.com>
2024-12-14 02:17:29 +0000

    fisrt commit

---
+++ cetes.py
@@ -0,0 +1,11 @@
+# INTERES COMPUESTO CETES
+def interes_compuesto(inicial,tasa,tiempo):
+ monto = inicial*(1+ tasa)**tiempo
+ return monto 
+i = 100 # monto inicial
+t = 0.10 # Tasa de interes
+tp = 3 # TIEMPO EN años
+
+monto_final = interes_compuesto(i,t,tp)
+print(f"El monto final despues de {tp} años sera: ${monto_final:.2f}")
+
