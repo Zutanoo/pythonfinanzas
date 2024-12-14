393460c87c6c2201d4d899297411710d68e290e4
Esteban <151067410+Zutanoo@users.noreply.github.com>
2024-12-13 20:31:04 -0700

    Actualizar cetes.py

--- cetes.py
+++ cetes.py
@@ -4,7 +4,7 @@ def interes_compuesto(inicial,tasa,tiempo):
  return monto 
 i = 100 # monto inicial
 t = 0.10 # Tasa de interes
-tp = 3 # TIEMPO EN años
+tp = 3 # TIEMPO EN añoss
 
 monto_final = interes_compuesto(i,t,tp)
 print(f"El monto final despues de {tp} años sera: ${monto_final:.2f}")
